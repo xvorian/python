4e2b36a1835366bb241b431116f1f933e85a5d71
POINTCLICKCARE\hanifa <hanifa@pointclickcare.com>
2023-04-12 16:13:04 -0400

    tuple and sets

--- learn/lists.py
+++ learn/lists.py
@@ -1,1 +1,31 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+# Create list
+numbers = [1, 2, 65, 3, 5, 67, 4, 3, 5, 6, 4, 2, 4, 5, 87, 3]
+numbers2 = list((43, 67, 67, 4, 3, 2, 56, 67, 87, 56, 3, 23, 45, 67, 89))
+
+# empty list
+my_list = []
+
+# list with mixed data types
+mix_list = [1, "Hello", 3.4]
+
+fruits = ["apple", "banana", "cherry", "apple", "cherry"]
+
+
+# print list
+print(numbers)
+print(numbers2)
+
+# list functions
+print(fruits[1].capitalize())
+print(fruits)
+print(len(fruits))
+
+# Append to list
+fruits.append('Mangos')
+print(len(fruits))
+print(fruits)
+
+#insert at certain position
+fruits.insert(1,'berries')
+print(fruits)
--- learn/tuples_sets.py
+++ learn/tuples_sets.py
@@ -1,5 +1,38 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
+tuples_numbers = (1, 2, 65, 3, 5, 67, 4, 3, 5, 6, 4, 2, 4, 5, 87, 3)
+tuples_numbers2 = tuple((43, 67, 67, 4, 3, 2, 56, 67, 87, 56, 3, 23, 45, 67, 89))
+
+# empty list
+tuples_my_list = ()
+
+# list with mixed data types
+mix_list = (1, "Hello", 3.4)
+
+tuples_fruits = ("apple", "banana", "cherry", "apple", "cherry")
+
+#print tuples
+print(tuples_numbers)
+print(tuples_numbers2)
+print(type(tuples_fruits))
+
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+set_numbers = {1, 2, 65, 3, 5, 67, 4, 3, 5, 6, 4, 2, 4, 5, 87, 3}
+# set_numbers2 = set{43, 67, 67, 4, 3, 2, 56, 67, 87, 56, 3, 23, 45, 67, 89}
+set_my_list = {}
+
+# list with mixed data types
+set_mix_list = {1, "Hello", 3.4}
+set_fruits = {"apple", "banana", "graphs", "apple", "cherry"}
+
+
+# check if in set
+print('Apple' in set_fruits)
+
+print(set_numbers)
+print(set_fruits)
+print(type(set_fruits))
+set_fruits.add('watermalon')
+print(set_fruits)
+
