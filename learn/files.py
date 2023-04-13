31d5dc5b4619811da521631a05dfe46f922d16b0
Amjad Hanif <amjad.hanif@hotmail.com>
2023-04-12 20:10:49 -0400

    update json

--- learn/files.py
+++ learn/files.py
@@ -1,1 +1,6 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+#Read from file
+myFile = open('sample.json','r+')
+text = myFile.read(1000)
+print(text)
--- learn/py_json.py
+++ learn/py_json.py
@@ -1,1 +1,7 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+import json
+
+userJSON = '{"first_name":"Amjad","last_name": "Hanif","age": 45,"active": "True"}'
+
+user = json.loads(userJSON)
+print(user)
