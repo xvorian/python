081e3166a30909846ca5c991b52b9334578078a9
Amjad Hanif <amjad.hanif@hotmail.com>
2023-06-23 23:21:41 -0400

    refactoring

--- basics/files.py
+++ basics/files.py
@@ -1,6 +1,6 @@
 # Python has functions for creating, reading, updating, and deleting files.
 
 #Read from file
-myFile = open('sample.json','r+')
+myFile = open('../data/sample.json', 'r+')
 text = myFile.read(1000)
 print(text)
--- basics/modules.py
+++ basics/modules.py
@@ -1,7 +1,5 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
 #Core modules
-import datetime
-from datetime import date
 
 from camelcase import CamelCase
 
@@ -15,7 +13,7 @@ from validator import validate_email
 # Using modules
 print(lists.fruits)
 
-print(functions.divide(3,4))
+print(functions.divide(3, 4))
 
 c = CamelCase()
 print(c.hump('all lower case'))
